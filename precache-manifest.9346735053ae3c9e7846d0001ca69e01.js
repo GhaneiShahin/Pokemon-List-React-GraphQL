438dfb6b22ef58a056ab229bb1a877bee2eaacb3
ShahinGH <ghaneishahin73@gmail.com>
2020-05-29 12:56:18 +0200

    Updates

---
+++ precache-manifest.9346735053ae3c9e7846d0001ca69e01.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c89dc356f64ed1e0307c61fbc6ed50",
+    "url": "/GhaneiShahin/Pokemon-List-React-GraphQL/index.html"
+  },
+  {
+    "revision": "a67f58a9a3dfb6ca569f",
+    "url": "/GhaneiShahin/Pokemon-List-React-GraphQL/static/css/2.e6cc0070.chunk.css"
+  },
+  {
+    "revision": "e0e051f2444de3e1dfbb",
+    "url": "/GhaneiShahin/Pokemon-List-React-GraphQL/static/css/main.6196441b.chunk.css"
+  },
+  {
+    "revision": "a67f58a9a3dfb6ca569f",
+    "url": "/GhaneiShahin/Pokemon-List-React-GraphQL/static/js/2.46491e60.chunk.js"
+  },
+  {
+    "revision": "fcd9e2092bfa4145705a323cafabdfff",
+    "url": "/GhaneiShahin/Pokemon-List-React-GraphQL/static/js/2.46491e60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0e051f2444de3e1dfbb",
+    "url": "/GhaneiShahin/Pokemon-List-React-GraphQL/static/js/main.eae91808.chunk.js"
+  },
+  {
+    "revision": "0f412c9396492e0b0948",
+    "url": "/GhaneiShahin/Pokemon-List-React-GraphQL/static/js/runtime-main.b7619003.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/GhaneiShahin/Pokemon-List-React-GraphQL/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/GhaneiShahin/Pokemon-List-React-GraphQL/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/GhaneiShahin/Pokemon-List-React-GraphQL/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/GhaneiShahin/Pokemon-List-React-GraphQL/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/GhaneiShahin/Pokemon-List-React-GraphQL/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
